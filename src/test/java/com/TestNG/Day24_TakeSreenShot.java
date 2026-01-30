765261dd532ca3511cdad28b4db0eda5cadd361c
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2026-01-30 19:36:38 +0530

    Take Screen Shot

---
+++ src/test/java/com/TestNG/Day24_TakeSreenShot.java
@@ -0,0 +1,54 @@
+package com.TestNG;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Day24_TakeSreenShot {
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void before() {
+		driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+	}
+
+	@Test
+	public void test() throws Exception {
+
+// Step 1
+		TakesScreenshot tka = (TakesScreenshot) driver;
+
+		File sourceFile = tka.getScreenshotAs(OutputType.FILE);
+
+		File targetFile = new File(
+				"D:\\JBK\\Automation Module\\Pradeep_Selenium_Scripts\\TKA.JBKCRM\\target\\loginfail.png");
+
+		FileHandler.copy(sourceFile, targetFile);
+
+//Step 2
+		WebElement firstname = driver.findElement(By.xpath("//input[@placeholder='First Name']"));
+
+		File sourceFile1 = firstname.getScreenshotAs(OutputType.FILE);
+
+		File targetFile1 = new File(
+				"D:\\JBK\\Automation Module\\Pradeep_Selenium_Scripts\\TKA.JBKCRM\\target\\element.png");
+
+		FileHandler.copy(sourceFile1, targetFile1);
+	}
+
+	@AfterMethod
+	public void after() {
+		// driver.quit();
+	}
+}
